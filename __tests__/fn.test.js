4decfc3971e2fce13ffebc3744e4df7ff1a502e9
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-13 15:22:25 +0300

    initial commit

---
+++ __tests__/fn.test.js
@@ -0,0 +1,7 @@
+// @flow
+
+// import fn from '../src';
+
+// test('fn', () => {
+//   expect(fn(6)).toBe(3);
+// });
---
+++ src/bin/hexlet.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// import fn from '..';
+
+// console.log(half(Number(process.argv[process.argv.length - 1])));
