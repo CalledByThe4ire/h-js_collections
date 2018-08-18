a6b8edff2fd14428110686cc0a3d75e794318d11
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-18 21:03:18 +0300

    add solution for set

--- src/difference.js
+++ src/difference.js
@@ -1,1 +1,10 @@
 // @flow
+export default (set1: Set<T>, set2: Set<T>): Set<T> => {
+  const result: Set<T> = new Set();
+  set1.forEach((value: mixed) => {
+    if (!set2.has(value)) {
+      result.add(value);
+    }
+  });
+  return result;
+};
