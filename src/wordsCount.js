5e6bed8ec4d8a81be278a5107a62a9a7f893cf1b
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-18 11:38:07 +0300

    add hexlet' solution for map

--- src/wordsCount.js
+++ src/wordsCount.js
@@ -1,38 +1,11 @@
 // @flow
-export default (
-  words: Array<string>,
-  stopwords: Array<string>,
-): Map<K, V> => {
-  const normalizeCase = (list: Array<string>): Array<string> =>
-    list.map(item => item.toLowerCase());
-
-  const removeKeys = (
-    list: Array<string>,
-    keys: Array<string>,
-  ): Array<string> => {
-    let filtered = list;
-
-    for (const key of keys) {
-      filtered = filtered.filter(item => item !== key);
-    }
-    return filtered;
-  };
-
-  const countWordEntries = (list: Array<string>): Map<K, V> => {
-    const entriesList: Map<K, V> = new Map();
-
-    for (const key of list) {
-      if (!entriesList.has(key)) {
-        entriesList.set(key, list.filter(element => element === key).length);
+export default (words: Array<string>, stopWords: Array<string>): Map<K, V> =>
+  words
+    .map((word: string) => word.toLowerCase())
+    .filter((word: string) => !stopWords.includes(word))
+    .reduce((acc: Map<K, V>, word: string) => {
+      if (!acc.has(word)) {
+        return acc.set(word, 1);
       }
-    }
-
-    return entriesList;
-  };
-
-  const filteredWordslist: Array<string> = removeKeys(
-    normalizeCase(words),
-    normalizeCase(stopwords),
-  );
-  return countWordEntries(filteredWordslist);
-};
+      return acc.set(word, acc.get(word) + 1);
+    }, new Map());
