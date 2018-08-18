3dfaaf08e0b1627a58286cf9872ebb99fa4f14d8
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-18 08:54:57 +0300

    add hexlet' solution for array

--- src/uniq.js
+++ src/uniq.js
@@ -1,16 +1,8 @@
 // @flow
-export default (arr: Array<mixed>): Array<mixed> => {
-  const iter = (items: Array<mixed>, acc: Array<mixed>): Array<mixed> => {
-    const copy: Array<mixed> = items.slice();
+export default (coll: Array<mixed>): Array<mixed> =>
+  coll.reduce(
+    (acc: Array<mixed>, value: mixed): Array<mixed> =>
+      acc.includes(value) ? acc : acc.concat(value),
+    [],
+  );
 
-    if (copy.length === 0) {
-      return acc;
-    }
-
-    const head: mixed = copy.shift();
-    const filteredTail: Array<mixed> = copy.filter(item => item !== head);
-
-    return iter(filteredTail, acc.concat(head));
-  };
-  return iter(arr, []);
-};
