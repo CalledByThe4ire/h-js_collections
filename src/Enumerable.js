7e88ba80835c02d1060159e51275da9117411fbb
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-18 23:46:21 +0300

    add solution for fluent_interface

--- src/Enumerable.js
+++ src/Enumerable.js
@@ -4,11 +4,29 @@ class Enumerable {
   }
 
   // BEGIN (write your solution here)
-  // @flow
+  select(fn) {
+    this.collection = this.collection.map(fn);
+    return this;
+  }
   // END
 
   // BEGIN (write your solution here)
-  // @flow
+  orderBy(fn, direction = 'asc') {
+    const compare = (a, b) => {
+      const first = fn(a);
+      const second = fn(b);
+      if (first > second) {
+        return direction === 'asc' ? 1 : -1;
+      }
+      if (first < second) {
+        return direction === 'asc' ? -1 : 1;
+      }
+      return 0;
+    };
+
+    this.collection.sort(compare);
+    return this;
+  }
   // END
 
   where(fn) {
