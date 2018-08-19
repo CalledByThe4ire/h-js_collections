5af632451391dfdb7277990ef57e26079621a194
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-19 10:16:15 +0300

    add solution for immutable

--- src/Enumerable.js
+++ src/Enumerable.js
@@ -5,13 +5,31 @@ class Enumerable {
 
   select(fn) {
     // BEGIN (write your solution here)
-
+    const mapped = this.collection.map(fn);
+    return new Enumerable(mapped);
     // END
   }
 
   orderBy(fn, direction = 'asc') {
     // BEGIN (write your solution here)
+    const comparator = (a, b) => {
+      const first = fn(a);
+      const second = fn(b);
+      const compareResult = direction === 'asc' ? 1 : -1;
 
+      if (first > second) {
+        return compareResult;
+      }
+
+      if (first < second) {
+        return -compareResult;
+      }
+
+      return 0;
+    };
+
+    const sorted = this.collection.slice().sort(comparator);
+    return new Enumerable(sorted);
     // END
   }
 
