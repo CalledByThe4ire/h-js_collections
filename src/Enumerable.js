f0b8a96ff0a5456ce4fafd473df05a1f0ed4d655
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-19 10:07:41 +0300

    add exersice-unit for immutable

---
+++ src/Enumerable.js
@@ -0,0 +1,28 @@
+class Enumerable {
+  constructor(collection) {
+    this.collection = collection;
+  }
+
+  select(fn) {
+    // BEGIN (write your solution here)
+
+    // END
+  }
+
+  orderBy(fn, direction = 'asc') {
+    // BEGIN (write your solution here)
+
+    // END
+  }
+
+  where(fn) {
+    const filtered = this.collection.filter(fn);
+    return new Enumerable(filtered);
+  }
+
+  toArray() {
+    return this.collection;
+  }
+}
+
+export default Enumerable;
