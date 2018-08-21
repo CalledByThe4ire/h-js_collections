3c92421106a04810d2e3bdb31c83edf0146aec16
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-21 22:04:31 +0300

    add hexlet' solution for rest_operator

--- src/Enumerable.js
+++ src/Enumerable.js
@@ -8,23 +8,17 @@ class Enumerable {
     return new Enumerable(this.collection.slice(), this.operations.concat(fn));
   }
 
-  where(...args) {
-    return this.build((coll) => {
-      return args.reduce((acc, value) => {
-        if (typeof value === 'function') {
-          return acc.filter(value);
-        }
+  where(...predicates) {
+    const fns = predicates.map((predicate) => {
+      if (typeof predicate === 'function') {
+        return coll => coll.filter(predicate);
+      }
 
-        if (typeof value === 'object') {
-          return Object.entries(value).reduce((accumulator, item) => {
-            const key = item[0];
-            const val = item[1];
-            const func = element => element[key] === val;
-            return accumulator.filter(func);
-          }, acc);
-        }
-      }, coll);
+      const keys = Object.keys(predicate);
+      return coll => coll.filter(element =>
+        keys.every(key => predicate[key] === element[key]));
     });
+    return this.build(fns);
   }
 
   get length() {
