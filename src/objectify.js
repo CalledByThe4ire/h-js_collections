f0accc5d87066423707934209e877a5304da5d7c
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-25 14:36:54 +0300

    add hexlet' solution for spread_operator

--- src/objectify.js
+++ src/objectify.js
@@ -2,6 +2,5 @@
 type Obj = {[key: string]: mixed}
 type objCollection = {[key: string]: Obj};
 
-export default (list: Array<Obj>, fn: (item: Obj) => mixed): objCollection =>
-  list.reduce((acc: objCollection, item: Obj) =>
-    ({ ...acc, [fn(item)]: item }), {});
+export default (coll: Array<Obj>, select: (item: Obj) => mixed) =>
+  coll.reduce((acc: objCollection, item: Obj) => ({ ...acc, [select(item)]: item }), {});
