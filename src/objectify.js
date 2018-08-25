14b363f9ca02bd04fe442e5bfd32e0e6efa39daa
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-25 14:35:19 +0300

    add solution for spread_operator

--- src/objectify.js
+++ src/objectify.js
@@ -1,1 +1,7 @@
 // @flow
+type Obj = {[key: string]: mixed}
+type objCollection = {[key: string]: Obj};
+
+export default (list: Array<Obj>, fn: (item: Obj) => mixed): objCollection =>
+  list.reduce((acc: objCollection, item: Obj) =>
+    ({ ...acc, [fn(item)]: item }), {});
