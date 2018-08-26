1354d7ac15b9255f5b0553ae43b8f66db7332029
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-26 10:06:20 +0300

    add hexlet' solution for destructuring

--- src/getCarsCountByYear.js
+++ src/getCarsCountByYear.js
@@ -1,24 +1,16 @@
-/* eslint-disable no-confusing-arrow, no-return-assign, no-param-reassign */
-
 // @flow
-type initialObj = {[key: string]: number};
-type resultantObj = {[key: number]: number}
+type initialObj = { [key: string]: number };
+type resultantObj = { [key: number]: number };
 
-const getItemsCountByPropertyName = (list: Array<initialObj>, propName: string) => {
-  const mapped: Array<mixed> = list.map(item => item[propName]);
-
+export default (cars: Array<initialObj>) => {
   const iter = (items: Array<initialObj>, acc: resultantObj) => {
     if (items.length === 0) {
       return acc;
     }
-
-    const key: initialObj = items[0];
-    const value = list.reduce((accumulator: number, val: initialObj): number =>
-      val[propName] === key ? (accumulator += 1) : accumulator, 0);
-
-    return iter([...items.slice(1)], { ...acc, [key]: value });
+    const [{ year }, ...rest] = items;
+    const newValue = acc[year] ? acc[year] + 1 : 1;
+    return iter(rest, { ...acc, [year]: newValue });
   };
-  return iter(mapped, {});
-};
 
-export default (list: Array<initialObj>) => getItemsCountByPropertyName(list, 'year');
+  return iter(cars, {});
+};
