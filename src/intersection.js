5ed81ae1d73834f04c3b2b10fba4b24abb232916
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:22:11 +0300

    add hexlet' solution for intersection-challenge

--- src/intersection.js
+++ src/intersection.js
@@ -1,6 +1,5 @@
 // @flow
-export default (list1: Array<mixed>, list2: Array<mixed>): Array<mixed> => {
-  const filteredList1: Array<mixed> = Array.from(new Set(list1));
-  const filteredList2: Array<mixed> = Array.from(new Set(list2));
-  return filteredList1.filter(item => filteredList2.includes(item));
+export default (array1: Array<mixed>, array2: Array<mixed>): Array<mixed> => {
+  const filtered: Array<mixed> = array1.filter(value => array2.includes(value));
+  return [...new Set(filtered)];
 };
