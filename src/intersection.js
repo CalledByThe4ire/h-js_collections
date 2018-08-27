edf9a86cdc1087bd7b4adf659dab5cad102aaa56
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:20:35 +0300

    add solution for intersection-challenge

--- src/intersection.js
+++ src/intersection.js
@@ -1,1 +1,6 @@
 // @flow
+export default (list1: Array<mixed>, list2: Array<mixed>): Array<mixed> => {
+  const filteredList1: Array<mixed> = Array.from(new Set(list1));
+  const filteredList2: Array<mixed> = Array.from(new Set(list2));
+  return filteredList1.filter(item => filteredList2.includes(item));
+};
