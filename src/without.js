8b971dfba89c31f3d0a34cd4b913c8afc3fd2b51
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:37:04 +0300

    add hexlet' solution for without-challenge

--- src/without.js
+++ src/without.js
@@ -1,13 +1,5 @@
 // @flow
-export default (list: Array<mixed>, ...rest: Array<mixed>): Array<mixed> => {
-  const filteredList: Array<mixed> = Array.from(new Set(list));
-  const filteredRest: Array<mixed> = Array.from(new Set(rest));
-
-  return filteredRest.reduce((acc: Array<mixed>, item: mixed) => {
-    if (acc.includes(item)) {
-      const filtered = acc.filter((element: mixed) => element !== item);
-      return [...filtered];
-    }
-    return acc;
-  }, filteredList);
+export default (array: Array<mixed>, ...rest: Array<mixed>): Array<mixed> => {
+  const set = new Set(rest);
+  return array.filter(value => !set.has(value));
 };
