664cd61c0523ef0dde21010cae58042bae18d299
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:50:47 +0300

    add solution for from_pairs-challenge

--- src/fromPairs.js
+++ src/fromPairs.js
@@ -1,1 +1,12 @@
 // @flow
+type Obj = { [key: string]: number };
+type tuple = [string, number];
+
+export default (list: Array<tuple>) => {
+  const obj: Obj = {};
+  list.forEach((item: tuple) => {
+    const [key, value] = item;
+    obj[key] = value;
+  });
+  return obj;
+};
