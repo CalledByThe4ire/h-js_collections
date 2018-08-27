aa11f5a836ee3b1cec5ecc0dbbaee37fce83322c
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:36:15 +0300

    add solution for without-challenge

--- src/without.js
+++ src/without.js
@@ -1,1 +1,13 @@
 // @flow
+export default (list: Array<mixed>, ...rest: Array<mixed>): Array<mixed> => {
+  const filteredList: Array<mixed> = Array.from(new Set(list));
+  const filteredRest: Array<mixed> = Array.from(new Set(rest));
+
+  return filteredRest.reduce((acc: Array<mixed>, item: mixed) => {
+    if (acc.includes(item)) {
+      const filtered = acc.filter((element: mixed) => element !== item);
+      return [...filtered];
+    }
+    return acc;
+  }, filteredList);
+};
