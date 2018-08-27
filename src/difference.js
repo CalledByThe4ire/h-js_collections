231f3ed84ca52b8c35955a4b502d100207e47558
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:33:16 +0300

    add hexlet' solution for difference-challenge

--- src/difference.js
+++ src/difference.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-confusing-arrow */
 // @flow
-export default (list1: Array<mixed>, list2: Array<mixed>): Array<mixed> =>
-  list1.reduce(
-    (acc: Array<mixed>, item: mixed, idx: number) =>
-      item === list2[idx] ? acc : [...acc, item],
-    [],
-  );
+export default (array1: Array<mixed>, array2: Array<mixed>): Array<mixed> => {
+  const set = new Set(array2);
+  return array1.filter(value => !set.has(value));
+};
