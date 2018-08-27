6f5f546dc1ad1762ec912641b30cf622dfa798d2
Andrey Karamnoff <karamnoff@gmail.com>
2018-08-27 22:26:53 +0300

    add solution for difference-challenge

--- src/difference.js
+++ src/difference.js
@@ -1,1 +1,8 @@
+/* eslint-disable no-confusing-arrow */
 // @flow
+export default (list1: Array<mixed>, list2: Array<mixed>): Array<mixed> =>
+  list1.reduce(
+    (acc: Array<mixed>, item: mixed, idx: number) =>
+      item === list2[idx] ? acc : [...acc, item],
+    [],
+  );
