19f177f2477b24cee945e5e75f3bb8e7ec046c73
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-05 16:28:42 +0300

    add solution for enlarge_array_image-challenge

--- src/enlargeArrayImage.js
+++ src/enlargeArrayImage.js
@@ -1,1 +1,8 @@
 // @flow
+export default (list: Array<string[]>): Array<string[]> =>
+  list.reduce((acc, value, idx) => {
+    const doubled = [];
+    value.forEach(item => doubled.push(...item, ...item));
+    const resultingList = [...acc, (acc[idx] = doubled), (acc[idx + 1] = doubled)];
+    return resultingList;
+  }, []);
