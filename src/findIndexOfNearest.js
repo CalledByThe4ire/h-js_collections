7832f0a06f837a90fceb541688eabe0fc4efd7bb
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-05 16:43:33 +0300

    add solution for find_nearest_challenge

--- src/findIndexOfNearest.js
+++ src/findIndexOfNearest.js
@@ -1,1 +1,11 @@
 // @flow
+const findIndexOfNearest = (arr: Array<number>, n: number): number => {
+  if (arr.length === 0) {
+    return null;
+  }
+
+  const diff = arr.reduce((acc, value) => [...acc, Math.abs(value - n)], []);
+  return diff.findIndex(value => value === Math.min(...diff));
+};
+
+export default findIndexOfNearest;
