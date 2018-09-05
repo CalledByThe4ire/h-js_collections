d66eeb3e642c203f9f44f67590f9eb776fda4ef6
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-05 21:32:32 +0300

    add solution for findodd-challenge

--- src/findOdd.js
+++ src/findOdd.js
@@ -1,1 +1,11 @@
 // @flow
+export default (arr: Array<number>): number | null => {
+  for (const value of arr) {
+    const filtered = arr.filter(item => item === value);
+    if (filtered.length % 2 !== 0) {
+      return value;
+    }
+  }
+  return null;
+};
+
