1e23b7f9ff5d53459b74689b9b825ec4fb54dc4e
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-07 23:54:33 +0300

    add hexlet' solution for chunk-challenge

--- src/chunk.js
+++ src/chunk.js
@@ -1,25 +1,11 @@
 // @flow
-export default (arr: Array<any>, n: number): Array<Array<any>> => {
-  if (n === 0 || typeof n === 'undefined') {
-    return arr;
-  }
+export default (coll: Array<any>, size: number): Array<Array<any>> => {
+  const iter = (iterColl: Array<any>, acc: Array<Array<any>> = []) => {
+    if (iterColl.length === 0) {
+      return acc;
+    }
+    return iter(iterColl.slice(size), [...acc, iterColl.slice(0, size)]);
+  };
 
-  if (arr.length !== 0 && arr.length <= n) {
-    const resultingArray: Array<Array<any>> = [];
-    resultingArray[0] = arr;
-    return resultingArray;
-  }
-
-  let counter: number = 0;
-  let len: number = 0;
-  const chunksQuantity: number =
-    n % 2 === 0 ? Math.floor(arr.length / n) : Math.floor(arr.length / n) + 1;
-  const resultingArray: Array<Array<any>> = [];
-
-  while (counter < chunksQuantity) {
-    resultingArray[counter] = [...arr.slice(len, len + n)];
-    counter += 1;
-    len += n;
-  }
-  return resultingArray;
+  return iter(coll);
 };
