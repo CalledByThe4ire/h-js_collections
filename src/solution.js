03f3f8e2938508e05e1adc5c006f257cbb2ddb58
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-07 23:23:45 +0300

    add solution for nrzi-challenge

--- src/solution.js
+++ src/solution.js
@@ -1,1 +1,18 @@
 // @flow
+export default (str: string): string => {
+  if (str.length === 1 && str === '|') {
+    return '';
+  }
+
+  const filtered: Array<string> = str.split('').filter(item => item !== '|');
+  let symbol: string = filtered[0];
+  const mapped: Array<number> = filtered.map((item) => {
+    if (item === symbol) {
+      return 0;
+    }
+    symbol = item;
+    return 1;
+  });
+  return str[0] === '|' ? [1, ...mapped.slice(1)].join('') : mapped.join('');
+};
+
