0313a492b2163ee6e13edd64f040773d7ee42e8e
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-04 20:26:51 +0300

    add hexlet' solution for nrzi-challenge

--- src/solution.js
+++ src/solution.js
@@ -1,18 +1,10 @@
-// @flow
-export default (str: string): string => {
-  if (str.length === 1 && str === '|') {
-    return '';
-  }
-
-  const filtered: Array<string> = str.split('').filter(item => item !== '|');
-  let symbol: string = filtered[0];
-  const mapped: Array<number> = filtered.map((item) => {
-    if (item === symbol) {
-      return 0;
+export default str => str
+  .split('')
+  .map((e, i, arr) => {
+    if (e === '|') {
+      return e;
     }
-    symbol = item;
-    return 1;
-  });
-  return str[0] === '|' ? [1, ...mapped.slice(1)].join('') : mapped.join('');
-};
-
+    return i === 0 || e === arr[i - 1] ? 0 : 1;
+  })
+  .filter(e => e !== '|')
+  .join('');
