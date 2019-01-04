3b4f03bfd29f73ba23fcc40567e11b1f5e89027c
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-04 19:55:40 +0300

    add hexlet' solution for bracket_structure-challenge

--- src/isBracketStructureBalanced.js
+++ src/isBracketStructureBalanced.js
@@ -1,28 +1,29 @@
-// @flow
-const set: Set<string> = new Set(['{}', '()', '[]', '<>']);
+const brackets = new Map([['(', ')'], ['[', ']'], ['{', '}'], ['<', '>']]);
 
-const isAdmissibleChar = (str: string, coll: Set<string>) => {
-  const reduced = Array.from(coll).reduce(
-    (acc, value) => [...acc, `${value}`[0], `${value}`[1]],
-    [],
-  );
-  return Array.from(str).every((item: string) => reduced.includes(item));
+const isOpeningSymbol = symbol => brackets.has(symbol);
+
+const getClosingSymbolFor = symbol => brackets.get(symbol);
+
+const isStackEmpty = stack => stack.length === 0;
+
+export default str => {
+  const stack = [];
+  for (const symbol of str) {
+    if (isOpeningSymbol(symbol)) {
+      const closingSymbol = getClosingSymbolFor(symbol);
+      stack.push(closingSymbol);
+    } else {
+      if (isStackEmpty(stack)) {
+        return false;
+      }
+
+      const last = stack.pop();
+
+      if (symbol !== last) {
+        return false;
+      }
+    }
+  }
+
+  return isStackEmpty(stack);
 };
-
-const isBracketStructureBalanced = (str: string): boolean => {
-  if (str.length === 0) {
-    return true;
-  }
-
-  if (isAdmissibleChar(str, set) && set.has(`${str[0]}${str[1]}`)) {
-    return isBracketStructureBalanced(str.slice(2));
-  }
-
-  if (isAdmissibleChar(str, set) && set.has(`${str[0]}${str[str.length - 1]}`)) {
-    return isBracketStructureBalanced(str.slice(1, str.length - 1));
-  }
-
-  return false;
-};
-
-export default isBracketStructureBalanced;
