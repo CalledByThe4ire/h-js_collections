5916564730148b5ab63a38be2bbea211209c5b48
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-04 23:48:47 +0300

    add solution for data_normalization-challenge

--- src/normalizeData.js
+++ src/normalizeData.js
@@ -1,5 +1,25 @@
 import { eachDay, format } from 'date-fns';
 
 // BEGIN (write your solution here)
-
+export default (data, begin, end) => {
+  const [bY, bM, bD] = begin.split('-');
+  const [eY, eM, eD] = end.split('-');
+  const dateRange = eachDay(new Date(bY, bM, bD), new Date(eY, eM, eD)).map(
+    date => ({
+      value: 0,
+      date: `${format(
+        new Date(date.getFullYear(), date.getMonth() - 1, date.getDate()),
+        'DD.MM.YYYY'
+      )}`
+    })
+  );
+  data.forEach(input => {
+    dateRange.forEach(output => {
+      if (input.date === output.date) {
+        output.value = input.value;
+      }
+    });
+  });
+  return dateRange;
+};
 // END
